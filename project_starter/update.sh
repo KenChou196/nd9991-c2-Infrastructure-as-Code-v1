dee732997c127f1a68860e20a5fb4f9872920635
kynk1 <metal.bird.13@gmail.com>
2023-03-21 17:00:33 +0700

    update pj2

---
+++ project_starter/update.sh
@@ -0,0 +1,4 @@
+echo $1
+echo $2
+echo $3
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
